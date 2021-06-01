f273333dfa041bad763a8beb060402ffc983fb86
Aasiyafathima123 <80143359+Aasiyafathima123@users.noreply.github.com>
2021-06-01 15:39:58 +0530

    Add files via upload

---
+++ SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,37 @@
+var snow 
+var position ;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+
+  background(blue);  
+  
+ if(keyDown(LEFT_ARROW)) {
+writePosition(-1,0)
+}
+  
+if (keyDown(RIGHT_ARROW)){
+ writePosition(1,-0)
+
+ }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  drawSprites();
+}
